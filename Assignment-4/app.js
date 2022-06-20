304b41a8b26244dbf2562e5a2d17e3cc771ff8a6
Pamu1234 <sparmeshwar@qualminds.com>
2022-06-20 10:30:02 +0530

    Made changes in assignmet-4.

---
+++ Assignment-4/app.js
@@ -0,0 +1,9 @@
+let hamIcon = document.querySelector(".fa-bars");
+let display =document.querySelector("ul");
+console.log(display);
+hamIcon.addEventListener('click',()=>{
+    hamIcon.classList.toggle("fa-times")
+    display.classList.toggle("visible")
+})
+
+console.log("hello world");
